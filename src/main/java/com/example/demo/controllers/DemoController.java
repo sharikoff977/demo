968ffb5bc04d06b40a7755b37521f4edd7ca113a
drKashpirovskiy <d.sharovskiy@gmail.com>
2021-07-15 17:15:20 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/controllers/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Post;
+import com.example.demo.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    
+    @Autowired
+    private PostRepository postRepository;
+    
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "blog-main";
+    }
+
+}
